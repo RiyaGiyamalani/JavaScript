4c5fc19a4ef0cfe7a7a0aacdfd85aa7e19eec37f
Riya Giyamalani <riyagiyamalani@outlook.com>
2026-06-11 12:49:34 +0000

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; 
+// Treats the code as newer version of JS, 
+// though now it is not so necessary to write this but still a good practice
+
+// alert(3 + 3) is used in html and browser but not supported like this by node
+
+console.log( 
+    3 + 
+    3)       // will be executed but not a good practice, code readability should be high
+
+
+
+// tc39.es (ECMAScript) is the official documentation for JS 
+// but we'll prefer mdn (by mozilla) as it is easier to understand 
+
+
+let name = "Riya"
+let age = 3
+let isLoggedIn = true
+
+// Number => 2^53 - 1
+// BigInt
+// String
+// Boolean                                    // all these are primitive datatypes
+// null => stand-alone value
+// undefined
+// symbols => unique
+
+
+
+//object 
+
+
+
+console.log(typeof "Riya");
+console.log(typeof age);
+console.log(typeof null);         //shows object
+console.log(typeof undefined);         //shows undefined
