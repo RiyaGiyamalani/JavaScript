86d4c7c1bd23248dafd2cac22fdd95c97740d3d1
Riya Giyamalani <riyagiyamalani@outlook.com>
2026-06-12 12:00:58 +0000

    Stack and Heap Memory

--- 01_Basics/dataTypes_Summary.js
+++ 01_Basics/dataTypes_Summary.js
@@ -15,27 +15,59 @@ Instead, the JavaScript engine automatically determines the data type at runtime
 based on the value currently assigned to it.
 */
 
-const id1 = Symbol('124')
-const id2 = Symbol('124')
+// const id1 = Symbol('124')
+// const id2 = Symbol('124')
 
-console.log(id1 == id2);     //gives false
+// console.log(id1 == id2);     //gives false
 
-const bigNo = 18014398509481984n
+// const bigNo = 18014398509481984n
 
-console.log(typeof bigNo);      //gives bigInt
+// console.log(typeof bigNo);      //gives bigInt
 
-const names = ["abc", "def", "ghi", "jkl"]   //array
-let myObj = {
-    name : "Riya",
-    age : 20
-}                             //object
+// const names = ["abc", "def", "ghi", "jkl"]   //array
+// let myObj = {
+//     name : "Riya",
+//     age : 20
+// }                             //object
 
-const myFunction = function(){
-    console.log("Hello")
+// const myFunction = function(){
+//     console.log("Hello")
+// }
+
+
+// console.log(typeof myFunction);      //gives function but is actually object function
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// 2 types of memory: Stack and Heap
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubeName = "Scrolled Emotions"
+let anotherName = myYoutubeName
+
+anotherName = "Riya Recommends"
+
+console.log(anotherName);                   //Gives Riya Recommends
+console.log(myYoutubeName);                 //Gives Scrolled Emotions
+
+//Because it is copied, i.e. called by value, not by reference
+
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
 }
 
+let user2 = user1
 
-console.log(typeof myFunction);      //gives function but is actually object function
+user2.email = "user2@gmail.com"
 
+console.log(user1);          //{ email: 'user2@gmail.com', upi: 'user@ybl' }
+console.log(user2);          //{ email: 'user2@gmail.com', upi: 'user@ybl' }
+
+//Why? Called by Reference from the heap memory
 
 
